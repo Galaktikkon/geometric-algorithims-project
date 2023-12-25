f22ce79fac1b03f95155cdb66c26e6a08cd1def9
Jakub Kotara <jakubkotara@student.agh.edu.pl>
2023-12-25 20:51:57 +0100

    wszystko bez wizualizacji, dodalem funkcje w prostokacie

--- Rectangle.py
+++ Rectangle.py
@@ -1,5 +1,4 @@
-import Point
-import numpy as np
+from Point import Point
 
 
 class Rectangle:  # jeszcze nieprzydatne ale moze mi sie przyda do jakiejs wizualizacji czy cos
@@ -9,7 +8,24 @@ class Rectangle:  # jeszcze nieprzydatne ale moze mi sie przyda do jakiejs wizua
         self.dim = point1.dim
 
     def __str__(self):
-        return self.lowerLeft + ' - ' + self.upperRight
+        return str(self.lowerLeft) + ' - ' + str(self.upperRight)
 
-    def contains(self, point: Point):
+    def intersects(self, other):
+        return self.lowerLeft.precedes(other.upperRight) and self.upperRight.follows(other.lowerLeft)
+
+    def containsPoint(self, point: Point):
         return point.follows(self.lowerLeft) and point.precedes(self.upperRight)
+
+    def containsRect(self, other):
+        return other.lowerLeft.follows(self.lowerLeft) and other.upperRight.precedes(self.upperRight)
+
+    def divideRect(self, dim, divLine):
+        Lower = self.lowerLeft.data
+        Upper = self.upperRight.data
+        assert divLine <= Upper[dim] or divLine >= Lower[dim], 'Line does not belong to rectangle'
+        lowerIntersection, upperIntersection = list(
+            Lower), list(Upper)
+        lowerIntersection[dim] = upperIntersection[dim] = divLine
+        lowerIntersection, upperIntersection = tuple(
+            lowerIntersection), tuple(upperIntersection)
+        return (Rectangle(Point(Lower), Point(upperIntersection)), Rectangle(Point(lowerIntersection), Point(Upper)))
