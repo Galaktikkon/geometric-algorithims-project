0060fed821c1942958de268d600e521c040c63f7
Marek Malek <malekmarek56@gmail.com>
2023-12-31 00:12:43 +0100

    application structure fix

--- applicaton/widgets/testOptions.py
+++ applicaton/widgets/testOptions.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import ttk
 from widgets.pointsOptions import pointOptions
 from widgets.rectangleOptions import rectangleOptions
+from widgets.testName import testName
 
 
 class testOptions(LabelFrame):
@@ -16,8 +17,11 @@ class testOptions(LabelFrame):
         self.rectangleOptions: LabelFrame = rectangleOptions(self)
         self.rectangleOptions.grid(row=2, column=0, columnspan=2)
 
+        self.nameOptions: LabelFrame = testName(self)
+        self.nameOptions.grid(row=3, column=0, columnspan=2)
+
         self.clearButton: Button = ttk.Button(self, text="CLEAR")
-        self.clearButton.grid(row=3, column=0)
+        self.clearButton.grid(row=4, column=0)
 
         self.saveButton: Button = ttk.Button(self, text="SAVE")
-        self.saveButton.grid(row=3, column=1)
+        self.saveButton.grid(row=4, column=1)
